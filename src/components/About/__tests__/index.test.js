cbcd30d0fb48ba5a7796b5f98f358f8469fe9f29
Naomi-Eckhoff <nsuibhne@gmail.com>
2022-02-09 02:09:41 -0600

    c

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import About from '..';
+
+afterEach(cleanup);
+
+describe('About component', () => {
+
+  it('renders', () => {
+    render(<About />);
+  });
+
+  it('matches snapshot DOM node structure', () => {
+
+    const { asFragment } = render(<about />);
+
+    expect(asFragment()).toMatchSnapshot();
+
+  });
+
+})
